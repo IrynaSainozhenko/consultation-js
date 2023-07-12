350518b208900adb1a7f4d02b33efcce7ff2f48f
IrynaSainozhenko <zavatuhina@gmail.com>
2023-07-12 14:14:24 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("hehey");
+
+const arr1 = new Array();
+const arr2 = [];
+console.log(arr1);
+console.log(arr2);
+
+const fruits = ["apple", "orange", "mango"];
+const numbers = [9, 12, 18, 23];
+
+console.log(fruits);
+fruits[1] = "peach";
+console.log(fruits);
+
+console.log(fruits[2]);
+
+let temp = fruits[0];
+fruits[0] = fruits[fruits.length - 1];
+fruits[fruits.length - 1] = temp;
+
+const vegetables = ["carrot", "potato", "tomato"];
+const seventhElement = vegetables[7];
+let n5;
+console.log(n5);
+
+let n6 = 9;
+n6 = null;
+console.log(n6);
+
